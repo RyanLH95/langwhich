08f6500796ee800e79eb0cf3243e4e59d898e684
Ryan Henry <126239094+RyanLH95@users.noreply.github.com>
2023-07-31 14:11:35 +0100

    create multiple choice quiz, starting with a 'generateQuiz()' function with 'questions, quizContainer, resultsContainer and submitButton' as the parameters.

--- homepage/quiz/level1/question1.js
+++ homepage/quiz/level1/question1.js
@@ -1,10 +1,10 @@
 function generateQuiz(questions, quizContainer, resultsContainer, submitButton) {
 
     function showQuestions(questions, quizCOntainer){
-    
+       // this is for storing the output and the answer choices
         var output = [];
         var answers;
-
+          // for each question...
         for(var i=0; i<questions.length; i++){
             // first reset the list of answers
             answers = [];
@@ -12,7 +12,7 @@ function generateQuiz(questions, quizContainer, resultsContainer, submitButton) 
             // for each available answer to this question...
             for(word in questions[i].answers){
               
-                // ...add an html radio button
+                // this will add an html radio button
               answers.push(
                 '<label>'
                     + '<input type="radio" name="question'+i+'" value="'+letter+'">'
@@ -20,7 +20,7 @@ function generateQuiz(questions, quizContainer, resultsContainer, submitButton) 
               );
           }
          
-          // add this question and its answers to the output
+          // This adds question and its answers to the output
           output.push(
             '<div class="question"' + questions[i].question + '</div>'
             + '<div class="answers">' + answers.join('') + '</div>'
@@ -31,11 +31,41 @@ function generateQuiz(questions, quizContainer, resultsContainer, submitButton) 
     }
     
     function showResults(questions, quizContainer, resultsContainer){
+       
+        // gathers answer containers from our quiz
+        var answerContainers = quizContainer.querySelectorAll('.answers');
 
+        // keep tract of user's answers
+        var userAnswer = '';
+        var numCorrect = 0;
+
+        // for each question...
+        for(var i=0; i<questions.length; i++){
+
+            // find selected answer('||' operator is added to prevent quiz from breaking if USER does not complete question)
+            userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checkled')||{}).value;
+
+            //if answer is correct
+            if(userAnswer===questions[i].correctAnswer){
+                // add to the number of correct answers
+                numCorrect++;
+
+                // color the answers green
+                answerContainers[i].style.color = 'lightgreen';
+            }
+            // if answer is wrong or blank
+            else{
+                // color the answers red
+                answerContainers[i].style.color = 'red';
+            }
+        }
+        
+        // show num,ber of correct answers out of total
+        resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
     }
 
     showQuestions(questions, quizContainer);
-
+    // on submit, show results
     submitButton.onclick = function(){
         showResults(questions, quizContainer, resultsContainer);
     }
@@ -53,3 +83,8 @@ var myQuestions = [
         correctAnswer: "Bonjour!"
     }
 ]
+
+var quizContainer = document.getElementsByClassName('question');
+var resultsContainer = document.getElementByClassName('answers');
+
+generateQuiz(myQuestions, quizContainer, resultsContainer, submit);
