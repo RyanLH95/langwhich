6e5a4bed1dc642d0c028de9da7936206d7737063
Ryan Henry <126239094+RyanLH95@users.noreply.github.com>
2023-08-03 16:39:54 +0100

    Add 'startButton.addEventListener('click', () => {}) function' and add 'let currentQuestionIndex'

--- homepage/quiz/level1/question1.js
+++ homepage/quiz/level1/question1.js
@@ -1,3 +1,15 @@
-const startButton = document.getElementById('start-btn')
-const nextButton = document.getElementById('next-btn')
+// const variables for quiz elements 
+const startButton = document.getElementById('start-btn'); // returns an 'start-btn' id from HTML file.
+const nextButton = document.getElementById('next-btn'); // returns an 'next-btn' id from HTML file.
 const questionContainerElement = document.getElementById
+('question-container'); // returns an 'question-container' id from HTML file.
+const questionElement = document.getElementById('question'); // returns  an 'question' id from HTML file.
+const answerButtonsElement = document.getElementById
+('answer-buttons'); // returms an 'answer-buttons' id from HTML file.
+
+let currentQuestionIndex;
+
+startButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
