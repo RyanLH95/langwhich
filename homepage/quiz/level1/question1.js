ceea9f3c15cc14891f8ecbacfb2dd8daa2acba20
Ryan Henry <126239094+RyanLH95@users.noreply.github.com>
2023-08-07 14:46:33 +0100

    Add questions variable and create an array containing all questions for the quiz

--- homepage/quiz/level1/question1.js
+++ homepage/quiz/level1/question1.js
@@ -19,6 +19,7 @@ function startGame() {
     console.log('Started') // to check if the 'Start' button is being called when clicked.
     startButton.classList.add('hide') // to hide the start button
     questionContainerElement.classList.remove('hide') // this causes the 'question-container' to show
+    setNextQuestion()
 }
 
 function setNextQuestion() {
@@ -44,3 +45,51 @@ function setStatusClass() {
 function clearStatusClass() {
     
 }
+
+const questions = [
+    {
+        question: 'How do you say "Hello" in French?',
+        answers: [
+            { text: 'Bonjour!', correct: true },
+            { text: 'Au revoir!', correct: false },
+            { text: 'Quoi!', correct: false },
+            { text: 'Arrêt!', correct: false }
+        ]
+    },
+    {
+        question: 'How do say "How are you" in French',
+        answers: [
+            { text: 'Qui es-tu', correct: false },
+            { text: 'Que fais-tu', correct: false },
+            { text: 'Comment allez-vous', correct: true },
+            { text: 'Où vas-tu', correct: false }
+        ]
+    },
+    {
+        question: `Pick the most accurate repsonse to this question: "Salut comment tu t'appelles?"`,
+        answers: [
+            { text: `Je m'appelle`, correct: true },
+            { text: `Je vis dans`, correct: false },
+            { text: `C'est bon`, correct: false },
+            { text: `Me connaissez-vous`, correct: false }
+        ]
+    },
+    {
+        question: 'Pick the correct translation for this sentence: "May I have some water, Please?"',
+        answers: [
+            { text: `Donne moi de l'eau`, correct: false },
+            { text: `Oui je voudrais de l'eau`, correct: false },
+            { text: `Puis-je avoir de l'eau s'il vous plaît`, correct: true },
+            { text: `je veux de l'eau maintenant`, correct: false }
+        ]
+    },
+    {
+        question: `In this sentence: "Je m'appelle Jamie et j'habite à Paris", what does "j'habite" mean?`,
+        answers: [
+            { text: `We live`, correct: false },
+            { text: `I live`, correct: true },
+            { text: `I like`, correct: false },
+            { text: `I want`, correct: false }
+        ]
+    }
+]
