917b0a04ef434655fea18cdc7c5faad277e183fd
Ryan Henry <126239094+RyanLH95@users.noreply.github.com>
2023-08-12 14:43:33 +0100

    Move up 'quiz/' folder to langwhich folder next to index.html file

---
+++ quiz/level1/question1_fr.js
@@ -0,0 +1,130 @@
+// const variables for quiz elements 
+const startButton = document.getElementById('start-btn'); // returns an 'start-btn' id from HTML file.
+const nextButton = document.getElementById('next-btn'); // returns an 'next-btn' id from HTML file.
+const questionContainerElement = document.getElementById
+('question-container'); // returns an 'question-container' id from HTML file.
+const questionElement = document.getElementById('question'); // returns  an 'question' id from HTML file.
+const answerButtonsElement = document.getElementById
+('answer-buttons'); // returns an 'answer-buttons' id from HTML file.
+
+let shuffledQuestions, currentQuestionIndex; // to shuffle the question order and to know which question the user is currently on
+
+startButton.addEventListener('click', startGame) // When we click on startButton, it cues the code that's inside start
+
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
+
+function startGame() {
+    console.log('Started') // to check if the 'Start' button is being called when clicked.
+    startButton.classList.add('hide') // to hide the start button
+    shuffledQuestions = questions.sort(() => Math.random() - .5) // this method will help give a random array during the quiz.
+    currentQuestionIndex = 0 // This is for starting on the first questions with array
+    questionContainerElement.classList.remove('hide') // this causes the 'question-container' to show
+    setNextQuestion()
+}
+
+function setNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
+}
+
+function resetState() { // this function is for reset everything back to default state when initiating new question
+    clearStatusClass(document.body)
+     nextButton.classList.add('hide') // to hide next button unless called 
+     while (answerButtonsElement.firstChild) { // Add 'while' loop
+        answerButtonsElement.removeChild // if the there is child in answerButtonsElement, we want it removed
+        (answerButtonsElement.firstChild)
+     }
+}
+
+function selectAnswer(e) {
+     const selectedButton = e.target // 
+     const correct = selectedButton.dataset.correct 
+     setStatusClass(document.body, correct) // function to take 'document.body' for whether it should be set to correct or wrong
+     Array.from(answerButtonsElement.children).forEach(button => { // this needs to be an array so that we can convert for the forEach loop
+        setStatusClass(button, button.dataset.correct)
+     })
+     if (shuffledQuestions.length > currentQuestionIndex + 1) { // this means that we have more questions than we are currently on
+     nextButton.classList.remove('hide')
+    } else {
+      startButton.innerText = 'Restart'
+      startButton.classList.remove('hide')
+    }
+}
+
+function setStatusClass(element, correct) {
+     clearStatusClass(element)
+     if (correct) {
+        element.classList.add('correct')
+     } else {
+        element.classList.add('wrong')
+     }
+}
+
+function clearStatusClass() {
+    
+}
+
+const questions = [
+    {
+        question: `How do you say "Hello" in French?`,
+        answers: [
+            { text: `Bonjour!`, correct: true },
+            { text: `Au revoir!`, correct: false },
+            { text: `Quoi!`, correct: false },
+            { text: `Arrêt!`, correct: false }
+        ]
+    },
+    {
+        question: `How do you say "How are you" in French`,
+        answers: [
+            { text: `Qui es-tu`, correct: false },
+            { text: `Que fais-tu`, correct: false },
+            { text: `Comment allez-vous`, correct: true },
+            { text: `Où vas-tu`, correct: false }
+        ]
+    },
+    {
+        question: `Pick the most accurate repsonse to this question: "Salut comment tu t'appelles?"`,
+        answers: [
+            { text: `Je m'appelle`, correct: true },
+            { text: `Je vis dans`, correct: false },
+            { text: `C'est bon`, correct: false },
+            { text: `Me connaissez-vous`, correct: false }
+        ]
+    },
+    {
+        question: `Pick the correct translation for this sentence: "May I have some water, Please?"`,
+        answers: [
+            { text: `Donne moi de l'eau`, correct: false },
+            { text: `Oui je voudrais de l'eau`, correct: false },
+            { text: `Puis-je avoir de l'eau s'il vous plaît`, correct: true },
+            { text: `je veux de l'eau maintenant`, correct: false }
+        ]
+    },
+    {
+        question: `In this sentence: "Je m'appelle Jamie et j'habite à Paris", what does "j'habite" mean?`,
+        answers: [
+            { text: `We live`, correct: false },
+            { text: `I live`, correct: true },
+            { text: `I like`, correct: false },
+            { text: `I want`, correct: false }
+        ]
+    }
+]
